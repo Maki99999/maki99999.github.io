552ffc2ed65bbe193bca622f878827e3dc1d28c0
Maik <maki99999@web.de>
2025-04-13 16:37:17 +0200

    Revert "Deploy website - based on HEAD"
    This reverts commit 9be33eb67bbadfee1fe094a5a80cef8732ac37e6.

--- assets/js/acecf23e.d33fc70f.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/biome-beats/blog","blogTitle":"Blog","authorsListPath":"/biome-beats/blog/authors"}')}}]);
--- assets/js/dadb6317.0f11b030.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[9661],{7500:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/biome-beats/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
